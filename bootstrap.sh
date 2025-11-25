3af04bd8d0e607fefa048acb1c1359d41133aba0
Alex <alexanderthestudent@yandex.ru>
2025-11-25 20:16:52 +0300

    build: upgrade alpine to the edge repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 echo -e "c\n1" | setup-apkrepos
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk add erlang gleam rebar3 git
-apk add neovim zellij mprocs
+apk add zellij mprocs
 
 git clone https://github.com/snlxnet/api
 cd api
