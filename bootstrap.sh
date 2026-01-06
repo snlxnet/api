b72a547920d15a6bbe3a4543d00658f1c2c9ad1d
Alex <alexanderthestudent@yandex.ru>
2026-01-06 22:40:04 +0300

    build: update the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,11 @@ echo -e "c\n1" | setup-apkrepos
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
 echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-apk add erlang gleam rebar3 git
+apk add mprocs deno caddy
 
 cd
 rm -rf ./api # if it already exists
 git clone https://github.com/snlxnet/api
-cd api
 
-gleam export erlang-shipment
-./build/erlang-shipment/entrypoint.sh run
+cd api
+mprocs
