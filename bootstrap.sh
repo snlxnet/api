312875f538b1165c6377a997c269a8feb58576f2
Alex <alexanderthestudent@yandex.ru>
2026-01-10 16:51:08 +0300

    fix: add git

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ echo -e "c\n1" | setup-apkrepos
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
 echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-apk add mprocs deno caddy
+apk add mprocs deno caddy git
 
 cd
 rm -rf ./api # if it already exists
